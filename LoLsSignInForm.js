360cc95b8b414b19639c1d18207a294d28874763
Alyssa Pavao <alyssapavao@wlan232-101.wlan.wwu.edu>
2015-02-16 16:49:22 -0800

    SignIn Form update with google button

--- LoLsSignInForm.js
+++ LoLsSignInForm.js
@@ -3,6 +3,7 @@ function setupSignInForm(elem) {
 	'<div>' +
 	'	<form autocomplete="off">' +
 	'		<h3>Sign In Workspace</h3>' +
+	'		<button class ="zocial icon google" title = "Sign into Google" onclick="hello(\'google\').login()">Google Sign-in</button>' +
 	'		<button type="button" title="cancel" onClick="hideForm(\'SignInWorkbench\')">' +
 	'			Cancel' +
 	'		</button>' +
